89d7b9ebe5f1dda65c6f5a285ba0c719cc187f76
yusuke <theshoreunder@gmail.com>
2024-02-03 21:45:35 +0900

    Fix invalid value

--- google-cloud/private_sql.tf
+++ google-cloud/private_sql.tf
@@ -1,7 +1,7 @@
 # module "my-nat" {
 #   source      = "./module/private_sql"
 #   project_id  = google_project.project_one.project_id
-#   vpc         = "asia-northeast1"
+#   vpc         = google_compute_network.vpc_network.id
 #   subnet      = google_compute_subnetwork.my_subnet.self_link
 #   region      = "asia-northeast1"
 # }
